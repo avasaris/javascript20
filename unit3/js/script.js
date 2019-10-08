959cf33d52a7013c0db2d494d47e8d97d2276794
avasaris <artvas@yandex.ru>
2019-10-08 23:23:32 +0200

    three

--- unit3/js/script.js
+++ unit3/js/script.js
@@ -1,29 +1,14 @@
-let a = 6;
-let b = "Hello";
+const out = document.getElementById("out");
+const goButton = document.querySelector("button");
 
-let inputIn1 = document.querySelector('.input-in');
-let inputIn2 = document.querySelector('.input-in2');
-let button = document.querySelector('button');
-let divOut = document.querySelector('.out');
-let divOut2 = document.querySelector('.out2');
 
-// let y = document.querySelector('.input-2');
-// y.style = 'border: 2px solid red';
-// console.log(y);
+goButton.onclick = () => {
+    const in1 = document.getElementById("in1").value;
+    const in2 = document.getElementById("in2").value;
 
-console.clear();
-
-button.onclick = function () {
-    // console.log("Pushed");
-    // let b = +inputIn.value;
-    // let r = b * 10;
-    // divOut.innerHTML = "<b>" + r + "</b>";
-    // inputIn.value = "";
-    let i1 = +inputIn1.value;
-    let i2 = +inputIn2.value;
-    divOut.innerHTML = i1 + i2;
-    let i3 = parseInt(inputIn1.value);
-    let i4 = parseInt(inputIn2.value);
-    divOut2.innerHTML = i3 + i4;
-
+    if (in1 == in2) {
+        out.innerHTML = "equals";
+    } else {
+        out.innerHTML = Math.max(in1, in2);
+    }
 }
