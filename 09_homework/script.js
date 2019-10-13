62f638c2acb55ef1d16c053868d50ccfe81c7d07
avasaris <artvas@yandex.ru>
2019-10-13 22:31:08 +0200

    unit8 done

--- 09_homework/script.js
+++ 09_homework/script.js
@@ -1,4 +1,4 @@
-// TASK 1.  Напишите функцию func_1, которая присваивает p.u-1 ширину 200px.Проверьте ее работу.Допишите возможность присваивать высоту равную 100px;
+// TASK 1. Напишите функцию func_1, которая присваивает p.u-1 ширину 200px.Проверьте ее работу.Допишите возможность присваивать высоту равную 100px;
 
 function func_1(width = 100, height = 200) {
     const pu1Style = document.querySelector("p.u-1").style;
@@ -125,7 +125,7 @@ function func_10(elem) {
     document.querySelector("p.u-10__out").innerHTML = currencyFromUSD(value);
 }
 
-// TASK 11.Напишите функцию func_11, которая при клике на кнопке.u-11__button читает атрибут валюты data-currency и на основании этого выводит в p.u-11__out перевод валюты введенной пользователем в input.u-11__input в указанную валюту.Считается, что пользователь всегда вводит валюту в долларах. 
+// TASK 11. Напишите функцию func_11, которая при клике на кнопке.u-11__button читает атрибут валюты data-currency и на основании этого выводит в p.u-11__out перевод валюты введенной пользователем в input.u-11__input в указанную валюту.Считается, что пользователь всегда вводит валюту в долларах. 
 
 const buttons11 = document.querySelectorAll("button.u-11__button");
 
@@ -137,7 +137,7 @@ function func_11(elem) {
     document.querySelector("p.u-11__out").innerHTML = amount * currencyFromUSD(value);
 }
 
-// TASK  12. Создайте функцию func_12, которая создает через createElement элемент div, присваивает ему класс css-4 и возвращает данный элемент
+// TASK 12. Создайте функцию func_12, которая создает через createElement элемент div, присваивает ему класс css-4 и возвращает данный элемент
 
 function func_12() {
     const div12 = document.createElement("div");
@@ -148,21 +148,21 @@ function func_12() {
 console.log("<<< TASK 12. >>>");
 console.log(func_12());
 
-// TASK  13. Создайте функцию func_13, которая создает элемент span.span-13 с текстом 13 через createElement и вставляет его в p.u-13(append).
+// TASK 13. Создайте функцию func_13, которая создает элемент span.span-13 с текстом 13 через createElement и вставляет его в p.u-13(append).
 function createElement(type, cls, html) {
-    elem = document.createElement(type);
-    elem.setAttribute("class", cls);
+    const elem = document.createElement(type);
+    if (cls) elem.setAttribute("class", cls);
     elem.innerHTML = html;
 
     return elem;
 }
 
-// TASK  13.
+// TASK 13.
 
 function func_13() {
-    span13 = createElement("span", "span-13", 13);
+    const span13 = createElement("span", "span-13", 13);
 
-    pu13 = document.querySelector("p.u-13");
+    const pu13 = document.querySelector("p.u-13");
     pu13.append(span13);
     return pu13;
 }
@@ -170,12 +170,12 @@ function func_13() {
 console.log("<<< TASK 13. >>>");
 console.log(func_13());
 
-// TASK  14. Создайте функцию func_14, которая создает элемент span.span-14 с текстом 14 через createElement и вставляет его в p.u-14(prepend).
+// TASK 14. Создайте функцию func_14, которая создает элемент span.span-14 с текстом 14 через createElement и вставляет его в p.u-14(prepend).
 
 function func_14() {
-    span14 = createElement("span", "span-14", 14);
+    const span14 = createElement("span", "span-14", 14);
 
-    pu14 = document.querySelector("p.u-14");
+    const pu14 = document.querySelector("p.u-14");
     pu14.prepend(span14);
     return pu14;
 }
@@ -186,9 +186,9 @@ console.log(func_14());
 // TASK 15. Создайте функцию func_15, которая создает элемент span.span-15 с текстом 15 через createElement и вставляет его в p.u-15(before)
 
 function func_15() {
-    span15 = createElement("span", "span-15", 15);
+    const span15 = createElement("span", "span-15", 15);
 
-    pu15 = document.querySelector("p.u-15");
+    const pu15 = document.querySelector("p.u-15");
     pu15.before(span15);
     return pu15;
 }
@@ -196,10 +196,10 @@ function func_15() {
 console.log("<<< TASK 15. >>>");
 console.log(func_15().parentElement);
 
-// TASK    16. Создайте функцию funct-16, которая создает элемент button.u-16 c текстом Push.Повесьте на данный элемент событие onclick со стрелочной функцией, которая в консоль выводит текст u-16. И после добавления события добавьте данный элемент на страницу в div.u-16__out.Проверьте работоспособность события.
+// TASK 16. Создайте функцию funct-16, которая создает элемент button.u-16 c текстом Push.Повесьте на данный элемент событие onclick со стрелочной функцией, которая в консоль выводит текст u-16. И после добавления события добавьте данный элемент на страницу в div.u-16__out.Проверьте работоспособность события.
 
 function func_16() {
-    b16 = document.createElement("button");
+    const b16 = document.createElement("button");
     b16.innerHTML = "Push";
     b16.onclick = () => {
         console.log("u-16");
@@ -211,16 +211,57 @@ document.querySelector("div.u-16__out").append(func_16());
 
 // TASK 17. Создайте функцию, funct-17, которая при запуске создаст элемент p c текстом 17 и заменит этим элементом div.u-17
 
-function func_17() { }
+function func_17() {
+    const p17 = createElement("p", "", 17);
 
-// TASK 19. C помощью цикла повесьте на div.out-18 функцию func_18. Данная функция дожна удалять элемент, на котором произошел клик из DOM.Функция должна возвращать удаленный элемент
+    const div17 = document.querySelector("div.u-17");
+    div17.replaceWith(p17);
+}
 
-function func_18() { }
-// TASK   19. Создайте функцию func_19, которая принимает параметр текст.Создает элемент li, вставляет в него указанный текст, и добавляет на страницу в ul.u-19 в конец списка.
+func_17();
 
-function func_19() { }
+// TASK 18. C помощью цикла повесьте на div.out-18 функцию func_18. Данная функция дожна удалять элемент, на котором произошел клик из DOM.Функция должна возвращать удаленный элемент
+
+const divs18 = document.querySelectorAll("div.out-18");
+
+setOnClickFunction(divs18, function () { console.log(func_18(this)); });
+
+function func_18(elem) {
+    elem.remove();
+    return elem;
+}
+
+// TASK 19. Создайте функцию func_19, которая принимает параметр текст.Создает элемент li, вставляет в него указанный текст, и добавляет на страницу в ul.u-19 в конец списка.
+
+function func_19(liText) {
+    const li = createElement("li", "", liText);
+
+    const ul19 = document.querySelector("ul.u-19");
+    ul19.append(li);
+}
+
+func_19("Test19");
+
 // TASK 20. Доработайте предыдущее задание.Допишите функцию func_20 которая может принимать текст от пользователя и вставлять в список ul.u-20. Также добавьте checkbox - важное, при этом созданный li получает класс.css-5.
 
-function func_20() { }
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+document.getElementById("b-20").onclick = () => { func_20(); };
+
+function func_20() {
+    const liText = document.getElementById("inp-20").value;
+
+    const li = document.createElement("li");
+    li.classList.add("css-5");
+    const randId=getRandomInt(100000,1000000);
+    li.innerHTML = `<input type="checkbox" id="${randId}" value="${liText}"><label for="${randId}">${liText}</label>`;
+
+    const ul20 = document.querySelector("ul.u-20");
+    ul20.append(li);
+}
 
 
