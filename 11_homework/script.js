a93b8be18abd9db617be5638a11802427179abd2
avasaris <artvas@yandex.ru>
2019-10-25 14:21:19 +0200

    hw11 task13 done

--- 11_homework/script.js
+++ 11_homework/script.js
@@ -99,7 +99,7 @@ function funcPush(arr) {
     const arrCopy = arr.slice();
 
     const newElem = document.querySelector('input.u-6').value;
-    arrCopy[arrCopy.length] = newElem;
+    if (newElem !== '') arrCopy[arrCopy.length] = newElem;
 
     return arrCopy;
 }
@@ -145,7 +145,7 @@ printArray(arr8, 'div.out-8');
 function funcShift(arr) {
     const retArr = [];
 
-    for(let i=1; i<arr.length; i++) retArr[i-1]=arr[i];
+    for (let i = 1; i < arr.length; i++) retArr[i - 1] = arr[i];
 
     return retArr;
 }
@@ -170,7 +170,7 @@ printArray(arr9, 'div.out-9');
 function funcUnShift(inp, arr) {
     const retArr = [inp];
 
-    for(let i=0; i<arr.length; i++) retArr[i+1]=arr[i];
+    for (let i = 0; i < arr.length; i++) retArr[i + 1] = arr[i];
 
     return retArr;
 }
@@ -184,14 +184,91 @@ document.querySelector('button.u-9').onclick = () => {
 };
 
 /*
-Task 10.		Создайте массив arr10, примените к массиву метод reverse ( читать за метод reverse). После применения reverse, массив arr10 выподится в div.out-10.
+Task 10.		Создайте массив arr10, примените к массиву метод reverse (читать про метод reverse). После 
+                применения reverse, массив arr10 выподится в div.out-10.
 */
 
-let arr10 = [2,4, 6, 8, 10, 'hello'];
+const arr10 = [2, 4, 6, 8, 10, 'hello'];
 
-printArray(arr10,'div.out-10');
+printArray(arr10, 'div.out-10');
 
 document.querySelector('button.u-10').onclick = () => {
     arr10.reverse();
-    printArray(arr10,'div.out-10');
+    printArray(arr10, 'div.out-10');
+};
+
+/*
+Task 11.		Создайте input.u-11__input и кнопку button.u-11__button. Объявите массив arr11. По нажатию 
+                на кнопку, читайте содержимое input.u-11__input в переменную u11. Потом примените к массиву arr11 
+                метод indexOf (читать про метод indexOf). Результат применения indexOf выводите на страницу в div.out-11. 
+                Последовательно проверьте программу вводя в input числа 1, 3, 5, 12.
+*/
+
+const arr11 = [0, 2, 3, 7, 8, 5, 11];
+
+document.querySelector('button.u-11__button').onclick = () => {
+    let u11 = document.querySelector('input.u-11__input').value;
+    if (u11 !== '') u11 = +u11;
+
+    const ind = arr11.indexOf(u11);
+
+    document.querySelector('div.out-11').innerHTML = ind;
+};
+
+/*
+Task 12.		Напишите функцию funcIndexOf, которая эмулирует работу метода indexOf. Программа должна:
+                    - читать содержимое input в переменную;
+                    - запускать цикл по массиву и сравнивать каждый элемент массива с введенным;
+                    - если совпадение есть - останавливать цикл и выводить номер индекса на котором произошло совпадение;
+                    - если совпадения нет выводить -1.
+*/
+
+const arr12 = [0, 2, 3, 7, 8, 5, 11];
+
+function funcIndexOf(elem, arr) {
+    let ret = -1;
+
+    let match = false;
+    let i = 0;
+    while (!match && i < arr.length) {
+        match = arr[i] === elem;
+        i += 1;
+    }
+    if (match) ret = i - 1;
+
+    return ret;
 }
+
+document.querySelector('button.u-12__button').onclick = () => {
+    let u12 = +document.querySelector('input.u-12__input').value;
+    if (u12 !== '') u12 = +u12;
+
+    const ind = funcIndexOf(u12, arr12);
+
+    document.querySelector('div.out-12').innerHTML = ind;
+};
+
+/*
+Task 13.		Напишите функцию funcReverse, которая эмулирует работу метода reverse. Программа должна:
+                    - cоздать новый пустой массив;
+                    - перебирать старый массив с конца (массив создайте сами);
+                    - по очереди по элементу присвоить значения в новый массив.
+                Выведите новый массив.
+*/
+
+let arr13 = [2, 7, 'ddd', 8, 10, 'hello', {aa:"bb"}, [1,2,3]];
+
+printArray(arr13, 'div.out-13');
+
+function funcReverse(arr){
+    const retArr = [];
+
+    for(let i=arr.length-1; i>=0; i--) retArr.push(arr[i]);
+
+    return retArr;
+}
+
+document.querySelector('button.u-13').onclick = () => {
+    arr13 = funcReverse(arr13);
+    printArray(arr13, 'div.out-13');
+};
