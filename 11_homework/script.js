8bcac1aea06b4bda3bbdd3fe49fe326bc6a53cd8
avasaris <artvas@yandex.ru>
2019-10-29 09:45:14 +0200

    13

--- 11_homework/script.js
+++ 11_homework/script.js
@@ -7,6 +7,12 @@ function printArray(arr, selector) {
     document.querySelector(selector).innerHTML = out;
 }
 
+function getRandomInt(min, max) {
+    const newMin = Math.ceil(min);
+    const newMax = Math.floor(max);
+    return Math.floor(Math.random() * (newMax - newMin)) + newMin;
+}
+
 /* 
 Task 1.     Создайте пустой массив arr1, input.u-1, кнопку button.u-2__push. При нажатии кнопки, 
             функция читает содержимое input и добавляет содержимое в массив, после чего выводит 
@@ -256,14 +262,14 @@ Task 13.		Напишите функцию funcReverse, которая эмули
                 Выведите новый массив.
 */
 
-let arr13 = [2, 7, 'ddd', 8, 10, 'hello', {aa:"bb"}, [1,2,3]];
+let arr13 = [2, 7, 'ddd', 8, 10, 'hello', { aa: 'bb' }, [1, 2, 3]];
 
 printArray(arr13, 'div.out-13');
 
-function funcReverse(arr){
+function funcReverse(arr) {
     const retArr = [];
 
-    for(let i=arr.length-1; i>=0; i--) retArr.push(arr[i]);
+    for (let i = arr.length - 1; i >= 0; i--) retArr.push(arr[i]);
 
     return retArr;
 }
@@ -271,4 +277,151 @@ function funcReverse(arr){
 document.querySelector('button.u-13').onclick = () => {
     arr13 = funcReverse(arr13);
     printArray(arr13, 'div.out-13');
+};
+
+/*
+Task 14.        Создайте инпут, куда пользователь может ввести число элементов в массиве. Создайте функцию,
+                которая прочитает введенное число и создаст массив такой длины, причем каждый элемент - это 
+                случайное число от 0 до 100. Массив выведите на страницу.
+*/
+
+function generateRandomArr(len) {
+    const retArr = [];
+
+    for (let i = 0; i < len; i++) retArr[i] = getRandomInt(0, 100);
+
+    return retArr;
+}
+
+document.querySelector('button.u-14').onclick = () => {
+    const len14 = +document.querySelector('input.u-14').value;
+
+    const arr14 = generateRandomArr(len14);
+
+    printArray(arr14, 'div.out-14');
+};
+
+/*
+Task 15.        Создайте массив arr15. Напишите функцию, которая создаст новый массив, в который входят только
+                четные элементы массива arr15 (элементы с четным индексом). Выведите на экран.
+*/
+
+function filterEven(arr) {
+    const retArr = [];
+
+    for (let i = 0; i < arr.length; i += 2) retArr.push(arr[i]);
+
+    return retArr;
+}
+
+document.querySelector('button.u-15').onclick = () => {
+    const randomLen = getRandomInt(5, 21);
+    const arr15 = generateRandomArr(randomLen);
+    printArray(arr15, 'div.out-15-arr1');
+
+    const arr15rez = filterEven(arr15);
+    printArray(arr15rez, 'div.out-15-arr2');
+};
+
+/*
+Task 16.        Создайте button.u-16__button. Объявите массив arr16_1 и arr16_2. По нажатию на кнопку примените
+                к массивам метод concat (читать про метод concat). 
+                Результат применения concat выводите на страницу в div.out-16.
+*/
+
+const arr16a = [3, 5, 7];
+const arr16b = [2, 4, 6];
+
+document.querySelector('button.u-16').onclick = () => {
+    const arr16 = arr16a.concat(arr16b);
+    printArray(arr16, 'div.out-16');
+};
+
+/*
+Task 17.        Напишите функцию funcConcat, которая эмулирует работу метода concat. Программа должна:
+                    - Перебирает второй массив и его элементы добавляет в первый массив.
+                    - Выводит массив на страницу.
+*/
+
+function myConcat(arrFirst, arrSecond) {
+    const retArr = arrFirst.slice();
+
+    for (let i = 0; i < arrSecond.length; i++) retArr.push(arrSecond[i]);
+    return retArr;
+}
+
+document.querySelector('button.u-17').onclick = () => {
+    const arr17a = generateRandomArr(getRandomInt(3, 5));
+    const arr17b = generateRandomArr(getRandomInt(3, 5));
+
+    document.querySelector('.arr17-1').innerHTML =
+        'let arr17_1 = ' + JSON.stringify(arr17a);
+    document.querySelector('.arr17-2').innerHTML =
+        'let arr17_2 = ' + JSON.stringify(arr17b);
+
+    const arr17 = myConcat(arr17a, arr17b);
+    printArray(arr17, 'div.out-17');
+};
+
+/*
+Task 18.        Создайте button.u-18__button и input.u-18__input. Объявите массив arr18 . 
+                По нажатию на кнопку примените к массивам метод includes, который проверяет 
+                есть ли в массиве значение введенное в input. (читать про метод includes). 
+                Результат применения includes выводите на страницу в div.out-18.
+*/
+
+const arr18a = [3, 5, 7, 11, 12, 13, 14];
+
+document.querySelector('button.u-18__button').onclick = () => {
+    let u18 = document.querySelector('input.u-18__input').value;
+    if (u18 !== '') u18 = +u18;
+
+    const isExist = arr18a.includes(u18);
+
+    document.querySelector('div.out-18').innerHTML = isExist;
+};
+
+/*
+Task 19.        Напишите функцию funcIncludes, которая эмулирует работу метода includes. Программа должна:
+                    - Перебирать массив и сравнивать если введенный элемент совпал с текущим - прекращать работу цикла и выводить true.
+                    - Если совпадений нет - false.
+*/
+
+const arr19a = [3, 5, 7, 11, 12, 13, 14];
+
+function funcIncludes(elem, arr) {
+    let ret = false;
+
+    for (let i = 0; i < arr.length; i++)
+        if (elem === arr[i]) {
+            ret = true;
+            break;
+        }
+
+    return ret;
+}
+
+document.querySelector('button.u-19__button').onclick = () => {
+    let u19 = document.querySelector('input.u-19__input').value;
+    if (u19 !== '') u19 = +u19;
+
+    const isExist = funcIncludes(u19, arr19a);
+
+    document.querySelector('div.out-19').innerHTML = isExist;
+};
+
+/*
+Task 20.        Объявите массив arr20. Создайте кнопку, по нажатию на которую к массиву 
+                применяется метод join (читать про метод join). Результат выведите на страницу.
+*/
+
+document.querySelector('button.u-20').onclick = () => {
+    const arr20 = generateRandomArr(getRandomInt(3, 10));
+    document.querySelector('pre.arr20').innerHTML =
+        'let arr20 = ' + JSON.stringify(arr20);
+
+    const separ = document.querySelector('input.u-20').value;
+
+    const joinedArr = arr20.join(separ);
+    document.querySelector('div.out-20').innerHTML = joinedArr;
 };
