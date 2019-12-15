3eb4ab2b730c55ae75d3233c8c921a0fa65a67a1
avasaris <artvas@yandex.ru>
2019-12-15 22:43:29 +0200

    hw 23 on the go

--- 23_homework/unit_23.js
+++ 23_homework/unit_23.js
@@ -2,86 +2,126 @@
 // Task 1 ============================================
 /* Создайте функцию t1 которая записывает  в LS  ключ 5 со значением 11. Проверьте что происходит при повторном вызове функции. Запускается ф-я по кнопкуе b-1. */
 
-function t1() {
+function t1(key, val) {
+    val = JSON.stringify(val);
+    localStorage.setItem(key, val);
+    console.log(localStorage);
 }
 
-// ваше событие здесь!!!
+document.querySelector('.b-1').onclick = () => {
+    t1(5, 11);
+}
 
 // Task 2 ============================================
 /* Создайте функцию t2 которая записывает  в LS  массив a2 = [7,6,5]. Ключ a2. Проверьте что происходит при повторном вызове функции. Запускается ф-я по кнопкуе b-2. */
 
-function t2() {
+let a2 = [7, 6, 5];
 
+function t2(key, val) {
+    t1(key, val);
 }
 
-// ваше событие здесь!!!
+document.querySelector('.b-2').onclick = () => {
+    t2('a2', a2);
+}
 
 
 // Task 3 ============================================
 /*  При нажатии кнопки t3 выведите из LS сохранненный массив a2. Выведите в out-3 в формате ключ пробел значение перенос строки.  */
 
-function t3() {
+function t3(key, out) {
+    let arr = JSON.parse(localStorage.getItem(key));
 
+    let rez = '';
+
+    arr && Object.entries(arr).forEach(([index, item]) => rez += `${index} ${item}<br>`);
+
+    document.querySelector(out).innerHTML = rez;
 }
 
-// ваше событие здесь!!!
+document.querySelector('.b-3').onclick = () => {
+    t3('a2', '.out-3');
+}
 
 
 // Task 4 ============================================
 /*  Создайте функцию t4 которая записывает  в LS  массив a4 = {hello: world, hi:mahai}. Ключ a4. Проверьте что происходит при повторном вызове функции. Запускается ф-я по кнопкуе b-4.*/
 
-function t4() {
+let a4 = { 'hello': 'world', 'hi': 'mahai' };
 
+function t4() {
+    t1('a4', a4);
 }
 
-// ваше событие здесь!!!
+document.querySelector('.b-4').onclick = t4;
 
 // Task 5 ============================================
 /*   При нажатии кнопки t5 выведите из LS сохранненный массив a24. Выведите в out-4 в формате ключ пробел значение перенос строки. */
 
 function t5() {
-
+    t3('a4', '.out-5');
 }
 
-// ваше событие здесь!!!
+document.querySelector('.b-5').onclick = t5;
 
 // Task 6 ============================================
 /*  Создайте функцию t6 которая очищает весь LS. Запуск по кнопке b-6*/
 
 function t6() {
-
+    localStorage.clear();
+    console.log(localStorage);
 }
 
-// ваше событие здесь!!!
+document.querySelector('.b-6').onclick = t6;
 
 
 // Task 7 ============================================
 /*  Создайте input i-7 куда пользователь может вводить числа и строки. Создайте массив a7. Когда пользователь нажимает кнопку b-7 число должно добавляться в массив. Массив должен сохраняться в LS с ключем a7.*/
 
-function t7() {
+let a7 = [];
 
+function t7() {
+    const newItem = document.querySelector('.i-7').value;
+    var regex = /^[0-9]+$/;
+    if (newItem.match(regex)) {
+        a7.push(+newItem);
+        t1('a7', a7);
+    }
+    console.log(localStorage);
 }
 
-// ваше событие здесь!!!
+document.querySelector('.b-7').onclick = t7;
 
 // Task 8 ============================================
 /*   Создайте функцию t8 при запуске которой из a7 удаляется последний элемент. После чего массив сохраняется в LS с ключем a7. Использовать массив из предыдущего задания. */
 
 function t8() {
-
+    a7.pop();
+    t1('a7', a7);
 }
 
-// ваше событие здесь!!!
-
+document.querySelector('.b-8').onclick = t8;
 
 // Task 9 ============================================
 /* Создайте 3 radiobutton c именем rb-9. Задайте для каждого value: #fff, #c0c0c0, #555. При изменении radibutton записывайте значение value в LS с ключем bg. Добавьте слушатель событий на изменение LS. Если есть ключ bg то при наступлении события изменять цвет фона на заданный в LS. */
 
 function t9() {
+    let bg = '';
+
+    document.querySelectorAll('input[name="rb-9"]').forEach(item => {
+        if (item.checked) bg = item.value;
+    });
+
+    if (bg.length > 0) {
+        localStorage.setItem('bg', bg);
+        document.getElementsByTagName('fieldset')[0].style.backgroundColor = bg;
+    }
 
 }
 
-// ваше событие здесь!!!
+document.querySelectorAll('input[name="rb-9"]').forEach(item => item.onclick = () => {
+    t9()
+});
 
 
 // Task 10 ============================================
